8cb59f659b5e7113d16134309875af0b80752820
Qard111 <ramon1000.com@gmail.com>
2021-10-30 17:54:42 +0300

    asas

---
+++ Data/MvcMoviesRContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMoviesR.Models;
+
+namespace MvcMoviesR.Data
+{
+    public class MvcMoviesRContext : DbContext
+    {
+        public MvcMoviesRContext (DbContextOptions<MvcMoviesRContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMoviesR.Models.Actor> Actor { get; set; }
+
+        public DbSet<MvcMoviesR.Models.Movie> Movie { get; set; }
+    }
+}
